0950dbcb0a8b09822cec7c97e7aa58e01d0a22a7
jotttt <jlaulik@hotmail.com>
2015-08-26 16:53:35 +0300

    building mobile view...

--- public/htdocs/js/main.js
+++ public/htdocs/js/main.js
@@ -7,12 +7,18 @@ $(document).ready(function () {
     'highlightOnScroll': true, //add class to heading that is currently in focus
     'highlightOffset': 1, //offset to trigger the next headline
 });
-
+	//INIT SCROLLBAR FOR MENU
 	$(window).load(function(){
 
 			$("#sidebar").mCustomScrollbar({
 				theme: "minimal"
 			});
         });
+
+	//MOBILE NAVBAR
+	var viewportWidth = $(window).width();
+	if (viewportWidth <= 768){
+		//$("#sidebar").addClass("slidx_menu");
+	}
 
 });
